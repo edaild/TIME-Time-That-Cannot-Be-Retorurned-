142f90f26027336c963a0533bf2d4eb9f0c43d29
edaild <ch5183sb@gmail.com>
2024-08-06 21:46:29 +0900

    Feat : Fork2

---
+++ TIME(되돌릴 수 없는 시간)/Assets/Script/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+
+public class GameManiser : MonoBehaviour
+{
+
+    public RectTransform[] spring = new RectTransform[4];       // Å« ÅÂ¿±
+    public RectTransform[] s_spring = new RectTransform[8];     // ÀÛÀº ÅÂ¿±
+    public float rotateSpeed;
+
+
+    private void Update()
+    {
+        if (spring != null)
+        {
+            spring[0].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+            spring[1].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+            spring[2].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+            spring[3].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+        }
+
+        if (s_spring != null)
+        {
+            s_spring[0].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+            s_spring[1].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+            s_spring[2].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+            s_spring[3].Rotate(new Vector3(0, 0, -rotateSpeed) * Time.deltaTime);
+
+            s_spring[4].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+            s_spring[5].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+            s_spring[6].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+            s_spring[7].Rotate(new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+
+        }
+    }
+
+    public void OnButtonclick_Gamestart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
